4da0c7a7e5d645bbb0917c12371dfc0920b89144
Will Luft <william.d.luft@gmail.com>
2020-02-03 18:38:34 -0500

    Started using hooks with Session.js component

--- src/components/Session.js
+++ src/components/Session.js
@@ -1,12 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-const Session = () => (
-  <div>
-    <h1>Session</h1>
-    <p>25:00</p>
-    <p>PLAY / PAUSE</p>
-    <p>RESET</p>
-  </div>
-);
+const Session = () => {
+  const [sessionLength, setSessionLength] = useState(1500);
+
+  const increaseLength = () => {
+    setSessionLength(sessionLength + 30);
+  };
+
+  const resetLegnth = () => {
+    setSessionLength(1500);
+  };
+
+  const showTime = () => {
+    let minutes = Math.floor(sessionLength / 60);
+    let seconds = sessionLength - minutes * 60;
+    if (seconds < 10) {
+      seconds = `0${seconds}`;
+    }
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    return `${minutes}:${seconds}`;
+  };
+
+  return (
+    <div>
+      <h1>Session</h1>
+      <p>{showTime()}</p>
+      <p>PLAY / PAUSE</p>
+      <button onClick={resetLegnth} type="button">
+        RESET
+      </button>
+      <button type="button" onClick={increaseLength}>
+        Increase Time
+      </button>
+    </div>
+  );
+};
 
 export default Session;
